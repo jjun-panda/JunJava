21d0eed5cdc3f27e6da6377adf60b1d98f235222
JJUN <jjuns.design@gmail.com>
2023-03-16 23:43:48 +0900

    0316 change

---
+++ HelloWorld1.java
@@ -0,0 +1,8 @@
+public class HelloWorld1 {
+  //main() 함수: 프로그램이 실행되는 진입점
+  public static void main(String[] args) {
+    // HelloWorld 출력해보자!
+    System.out.println("Hello World! Let's go");
+  }
+}
+
