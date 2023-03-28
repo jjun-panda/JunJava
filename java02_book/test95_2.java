68e753ab5c8e9a5b1d0dfdf993c7716138c99a91
JJUN <jjuns.design@gmail.com>
2023-03-28 19:48:55 +0900

    0328 add - homework#2

---
+++ java02_book/test95_2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class test95_2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        int max=0, mid=0, min=0;
+
+        System.out.print("정수 3개 입력: ");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        max = (a > b)? a : b;
+        max = (max > c)? max : c;
+
+        min = (a < b)? a : b;
+        min = (min < c)? min : c;
+
+        if (a == max) {
+            mid = (b > c)? b : c;
+        } else if (b == max) {
+            mid = (a > c)? a : c;
+        } else if (c == max) {
+            mid = (a < b)? a : b;
+        }
+
+        System.out.printf("max=%d, mid=%d, min=%d\n", max, mid, min);
+
+        scan.close();
+    }
+}
